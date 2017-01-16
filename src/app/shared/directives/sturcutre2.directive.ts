5e76469c4cff36b7c0a641cdb8e6d269e210b275
Zhentian Wan <answer881215@gmail.com>
2017-01-16 13:02:30 +0200

    update context

--- src/app/shared/directives/sturcutre2.directive.ts
+++ src/app/shared/directives/sturcutre2.directive.ts
@@ -5,9 +5,14 @@ import {Directive, Input, TemplateRef, ViewContainerRef} from "@angular/core";
 export class ThreeDirective {
   @Input() set three(value) {
     let num = 3;
+
     while (num--) {
+      const message = {
+        to: "People" + Math.random(),
+        message: "Hello" + Math.random()
+      };
       this.view.createEmbeddedView(this.template, {
-        $implicit: 'Awesome'
+        $implicit: message
       })
     }
   }
